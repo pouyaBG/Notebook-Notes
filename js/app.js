7f1af4ed419b5ef72abebce7e1d65989baca9629
pouya <b_pooya@yahoo.com>
2022-03-21 15:40:06 +0330

    classNote

--- js/app.js
+++ js/app.js
@@ -11,6 +11,12 @@ const notes = [
     body: 'some dummy text firt',
     updated: '2021-10-31T15:03:23.556Z',
   },
+  {
+    id: 12,
+    titile: 'second note-esssssdeu',
+    body: 'some dummy text second=sad',
+    updated: '2021-10-31T15:04:00.411Z',
+  },
 ];
 
 // take note / save note / delete note
@@ -22,6 +28,30 @@ class NotesAPI {
       return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
     });
   }
-  saveNote() {}
-  deleteNote() {}
+
+  static saveNote(noteToSave) {
+    //1.exsist
+    //2.not exsist
+    const notes = NotesAPI.getAllNotes();
+    // use id
+    const exsistNote = notes.find((n) => n.id == noteToSave.id);
+    if (exsistNote) {
+      //user send
+      exsistNote.titile = noteToSave.titile;
+      exsistNote.body = noteToSave.body;
+      exsistNote.updated = new Date().toISOString();
+    } else {
+      noteToSave.id = new Date().getTime();
+      noteToSave.updated = new Date().toISOString();
+      // title ok body ok =>new note
+      notes.push(noteToSave);
+    }
+    localStorage.setItem('notes-app', JSON.stringify(notes));
+  }
+
+  static deleteNote(id) {
+    const notes = NotesAPI.getAllNotes();
+    const fillterNotes = notes.filter((n) => n.id != id);
+    localStorage.setItem('notes-app', JSON.stringify(fillterNotes));
+  }
 }
