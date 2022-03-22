33fbbbe574f2183f4d48672bf09684c7661592a0
pouya <b_pooya@yahoo.com>
2022-03-22 15:24:40 +0430

    JS

--- js/app.js
+++ js/app.js
@@ -2,11 +2,16 @@ import NotesAPI from './NotesAPI.js';
 import NotesView from './NotesView.js';
 
 const app = document.getElementById('app');
-const viwe = new NotesView(app, {
+const view = new NotesView(app, {
   onNoteAdd() {
     console.log('Note added');
   },
   onNoteEdit(newTitle, newBody) {
     console.log(newTitle, newBody);
   },
+  onNoteSelect(noteId) {
+    console.log(noteId);
+  },
 });
+
+view.updateNoteList(NotesAPI.getAllNotes())
