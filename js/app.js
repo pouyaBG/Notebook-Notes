298f984fe6dfca371a5ae547516d1144fd6cd351
pouya <b_pooya@yahoo.com>
2022-03-23 12:45:58 +0430

    change

--- js/app.js
+++ js/app.js
@@ -1,22 +1,52 @@
 import NotesAPI from './NotesAPI.js';
 import NotesView from './NotesView.js';
 
-const app = document.getElementById('app');
-const view = new NotesView(app, {
-  onNoteAdd() {
-    console.log('Note added');
-  },
-  onNoteEdit(newTitle, newBody) {
-    console.log(newTitle, newBody);
-  },
-  onNoteSelect(noteId) {
-    console.log(noteId);
-    // make note
-  },
-  onNodeDelet(noteId) {
-    console.log(noteId);
-  },
-});
+export default class App {
+  constructor(root) {
+    this.notes = [];
+    this.activNote = null;
+    this.view = new NotesView(root, this._handlers());
+    this._refreshNotes();
+  }
+  _refreshNotes() {
+    const notes = NotesAPI.getAllNotes();
 
-view.updateNoteList(NotesAPI.getAllNotes());
-view.updateActiveNote(NotesAPI.getAllNotes()[1]);
+    // set notes:
+    this.notes = notes;
+    this.view.updateNoteList(notes);
+    this.view.updateNotePreviewVissibility(notes.length > 0);
+
+    // set active notes:
+    this.activNote = notes[0];
+    this.view.updateActiveNote(notes[0]);
+  }
+  _handlers() {
+    return {
+      onNoteAdd: () => {
+        const newNote = {
+          title: 'new note',
+          body: ' take new note',
+        };
+        NotesAPI.saveNote(newNote);
+        this._refreshNotes();
+      },
+      onNoteEdit: (newTitle, newBody) => {
+        NotesAPI.saveNote({
+          id: this.activNote.id,
+          title: newTitle,
+          body: newBody,
+        });
+        this._refreshNotes();
+      },
+      onNoteSelect: (noteId) => {
+        const selectedNote = this.notes.find((n) => n.id == noteId);
+        this.activNote = selectedNote;
+        this.view.updateActiveNote(selectedNote);
+      },
+      onNodeDelet: (noteId) => {
+        NotesAPI.deleteNote(noteId);
+        this._refreshNotes();
+      },
+    };
+  }
+}
